0345466afcb9ea9547b15f4123140e18c1c6817f
Caitlin (cara) <cara@odoo.com>
2023-06-21 14:52:40 -0400

    [ADD] Stock picking model to override for registry

---
+++ motorcycle_registry_GE2/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Motorcycle Registry GE02",
+    
+    "summary": "Motorcyle Registry Enhancements",
+    
+    "description": """
+    GE02 Motorcycle Registry Enhancements
+    """,
+    
+    "version": "0.2",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry_GE1"],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
