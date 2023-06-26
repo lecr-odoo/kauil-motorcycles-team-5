3f4dbfdfaa2b23b4d04fb85515247ea609abe5b0
Huanfeng Cheng <huch@odoo.com>
2023-06-26 17:23:48 -0400

    [FIX] Changed registry_id defined in repair.order model to stored field.

--- motorcycle_registry_GE4/models/repair.py
+++ motorcycle_registry_GE4/models/repair.py
@@ -6,10 +6,10 @@ class Repair(models.Model):
 
     vin = fields.Char(string='VIN')
     current_mileage = fields.Float(string='current_mileage')
-    registry_id = fields.Many2one(comodel_name='motorcycle.registry', string='Registry ID', compute='_get_registry_id_from_vin')
+    registry_id = fields.Many2one(comodel_name='motorcycle.registry', string='Registry ID', compute='_get_registry_id_from_vin', store=True)
 
     # The following fields are overrode to be related to registry_id.
-    partner_id = fields.Many2one(relatiion='registry_id')
+    partner_id = fields.Many2one(relation='registry_id')
     sale_order_id = fields.Many2one(relation='registry_id')
     product_id = fields.Many2one(relation='registry_id')
 
